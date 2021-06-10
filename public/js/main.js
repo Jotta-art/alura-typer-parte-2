fbbe959205752929f620b3933e585a86d06cd3ee
João Pedro <joao.pedro42@outlook.com.br>
2021-06-10 09:04:31 -0300

    Game over com eventos

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(numPalavras);
 
 var campo = $(".campo-digitacao");
-campo.on("input", function() {
+campo.on("input", function () {
     var conteudo = campo.val();
 
     var qtdPalavras = conteudo.split(/\S+/).length - 1;
@@ -12,5 +12,17 @@ campo.on("input", function() {
 
     var qtdCaracteres = conteudo.length;
     $("#contador-caracteres").text(qtdCaracteres);
-})
+});
+
+var tempoRestante = $("#tempo-digitacao").text();
+campo.one("focus", function () {
+    var cronometroID = setInterval(function () {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+        if (tempoRestante < 1) {
+            campo.attr("disabled", true);
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+});
 
